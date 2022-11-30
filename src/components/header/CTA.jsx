b66490b567d23ae2b2781af8214fd0995bc5bbd8
Sheena Bonilla <sheena.myb@gmail.com>
2022-11-30 10:19:47 -0800

    Add pdf file and update href

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Resume from '../../assets/resume.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>CTA
-        <a href="">Download CV</a>
+        <a href={Resume} download>Download CV</a>
         <a href="#contact">Let's Talk</a>
     </div>
   )

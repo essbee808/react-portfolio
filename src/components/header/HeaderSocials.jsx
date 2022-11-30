d50498ffb3d771b1d1610c3b9f5d383ce5c27f5d
Sheena Bonilla <sheena.myb@gmail.com>
2022-11-30 14:25:28 -0800

    Add social icons and links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>HeaderSocials</div>
+    <div className='header_socials'>
+        <a href="https://www.linkedin.com/in/sheena-bonilla/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/essbee808" target="_blank"><FaGithub/></a>
+    </div>
   )
 }
 

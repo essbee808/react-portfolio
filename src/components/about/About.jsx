c966e15665d40167e114874aa3517f8909c4f7c7
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-01 13:58:59 -0800

    Add divs to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
 
 const About = () => {
   return (
     <section id="about">
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <img src={ME} alt="About Image" />
+        </div>
+
+        <div className="about_content">
+
+        </div>
+      </div>
     </section>
   )
 }

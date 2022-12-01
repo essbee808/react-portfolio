6b7fc26c645ef0690f715870e8a2286dab234d00
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-01 13:36:55 -0800

    Import useState hook and modify links

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,17 +4,17 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {SlUserFemale} from 'react-icons/sl'
 import {BiBook} from 'react-icons/bi'
 import {BiMessageSquareDetail} from 'react-icons/bi'
-// import {BiMessageSquareDetail} from 'react-icons/bi'
-
-// import {AiOutlineHome} from 'react-icons/ai'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><SlUserFemale/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#services"><BiMessageSquareDetail/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''}><SlUserFemale/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? 'active' : ''}><BiBook/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
 }

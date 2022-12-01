475fc8c6b071d8496834bd4fca5bf4d2a1f1fe17
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-01 09:52:33 -0800

    Add icons to nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {SlUserFemale} from 'react-icons/sl'
+import {BiBook} from 'react-icons/bi'
+
+// import {AiOutlineHome} from 'react-icons/ai'
 
 const Nav = () => {
   return (
     <nav>
-      <a href="#"></a>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><SlUserFemale/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#testimonials"><SlUserFemale/></a>
+      <a href="#contact"><SlUserFemale/></a>
+   
     </nav>
   )
 }

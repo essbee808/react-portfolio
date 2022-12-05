24089c288e337129d9d6c58e7dc7cde70b4917ee
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-04 18:59:21 -0800

    Delete components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,9 @@ import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
-
 
 const App = () => {
   return (
@@ -17,9 +14,7 @@ const App = () => {
         <Nav />
         <About />
         <Experience />
-        <Services />
         <Portfolio />
-        <Testimonials />
         <Contact />
         <Footer />
     </>

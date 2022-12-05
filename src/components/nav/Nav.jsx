786826a63f417c215ea1642e9660e67ce89df104
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-04 19:08:04 -0800

    Add divs and links for recent work

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {SlUserFemale} from 'react-icons/sl'
 import {BiBook} from 'react-icons/bi'
-import {BiMessageSquareDetail} from 'react-icons/bi'
+import {MdOutlineWorkOutline} from 'react-icons/md'
 import {useState} from 'react'
 
 const Nav = () => {
@@ -14,7 +14,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''}><SlUserFemale/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? 'active' : ''}><BiBook/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BiMessageSquareDetail/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdOutlineWorkOutline/></a>
     </nav>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image"></div>
+          <h3>Looking For</h3>
+          <a href="https://github.com/essbee808/looking-for">See more</a>
+        </article>
+
+        <article className="portfolio_item">
+          <div className="portfolio_item-image"></div>
+          <h3>Factivity</h3>
+          <a href="https://github.com/essbee808/factivity">See more</a>
+        </article>
+      </div>
+    </section>
   )
 }
 

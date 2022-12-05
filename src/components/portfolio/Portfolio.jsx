092adbf3684597cd0b0e2be45fd1309fc792773e
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-04 20:17:50 -0800

    Refactor portfolio container and create tags dynamically

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,21 @@ import './portfolio.css';
 import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
 
+const data = [
+  {
+      id: 1,
+      image: IMG1,
+      title: 'Looking For',
+      github: 'https://github.com/essbee808/looking-for'
+  },
+  {
+      id: 2,
+      image: IMG2,
+      title: 'CaliGolf',
+      github: 'https://github.com/essbee808/cali-golf-api'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -10,25 +25,21 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio_container">
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>Looking For</h3>
-          <div className="portfolio_item-cta">
-            <a href="https://github.com/essbee808/looking-for" target="_blank" className="btn btn-primary">GitHub</a>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>CaliGolf</h3>
-          <div className="portfolio_item-cta">
-            <a href="https://github.com/essbee808/cali-golf-api" target="_blank" className="btn btn-primary">GitHub</a>
-          </div>
-        </article>
+        {
+          data.map(({id, image, title, github}) => {
+            return (
+              <article key={id} className="portfolio_item">
+                <div className="portfolio_item-image">
+                  <img src={image} alt={title} />
+                </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                <a href={github} target="_blank" className="btn btn-primary">GitHub</a>
+              </div>
+              </article>
+              )
+          })
+        }
       </div>
     </section>
   )
